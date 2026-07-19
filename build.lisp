bf0e51a9e649e38dbb299e8f3af5cfa98f79497e
di <git@diferrari.com>
2026-07-19 00:00:00 +0000

    [BUILD] dyn-target for better cross comp

--- build.lisp
+++ build.lisp
@@ -1,7 +1,7 @@
 (load "~/redbuild/v3/redbuild.lisp")
 (load "~/redbuild/packaging/packager.lisp")
 
-; (redbuild:set-tester "tester.c")
+(redbuild:set-global-target :linux)
 
 (redbuild:make "~/redlib" "cross")
 (redbuild:build-dep "~/beyond")
@@ -10,18 +10,18 @@
 (redbuild:quick-build (redbuild:make-instance `redbuild:redmod
         :name "brain"
         :type :bin
-        :target :redacted
+        :target (redbuild:dyn-target)
         :libs (list (redbuild:local-lib "uno" :lib "uno.a") (redbuild:local-lib "beyond" :lib "imaginal.a"))
         :srcs (list "brain.c" "tree_layout.c" "file/file_source.c" "shell/shell_source.c")
-) :add-dependencies t :run nil :debug-symbols t :success (lambda () 
+) :add-dependencies t :run t :debug-symbols t :success (lambda () 
     (print (redbuild:emit-compile-commands))
-    (generate-build (make-instance `pack
-        :name "brain"
-        :version "0.1a1"
-        :author "di"
-        :categories "Developer"
-    ) :redacted)
-    (redbuild:install "brain.red")
-    (redbuild:make "~/os" "run")
+    ; (generate-build (make-instance `pack
+    ;     :name "brain"
+    ;     :version "0.1a1"
+    ;     :author "di"
+    ;     :categories "Developer"
+    ; ) :redacted)
+    ; (redbuild:install "brain.red")
+    ; (redbuild:make "~/os" "run")
     (print "Done")
 ))
